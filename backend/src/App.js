1442bd701af22e44df5996130955961772e92d78
Prasann Lasod <prasannlasod0009@gmail.com>
2025-03-21 21:26:43 +0530

    cors update

--- backend/src/App.js
+++ backend/src/App.js
@@ -8,6 +8,27 @@ app.use(cors());
 
 app.use(express.json());
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  process.env.FRONTEND_URL, // Allow production frontend URL
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
