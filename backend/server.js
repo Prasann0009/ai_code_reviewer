b80582b6f6b6bf6b54774f2639a3988143dca4c6
Prasann Lasod <prasannlasod0009@gmail.com>
2025-03-22 09:23:02 +0530

    add port in server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const app = require("./src/App");
 
-app.listen(3000, () => {
-  console.log("server is running on URL:- http://localhost:3000");
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port:- ${PORT}`);
 });
